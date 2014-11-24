12fd0b40798a193debf555cb150c1d066826e5ae
Arpit Jain <arpit.jain2811@gmail.com>
2014-11-24 16:30:29 -0500

    bug bug

--- src/edu/nyu/cs/cs2580/PRF.java
+++ src/edu/nyu/cs/cs2580/PRF.java
@@ -61,12 +61,14 @@ public class PRF {
 		for(i=0;i<scoreTerms.size();i++)
 			scoreTerms.get(i).set_score(scoreTerms.get(i).get_score()/Total);
 		
-		
+		System.out.println(Total);
 		Vector<ScoredTerms> scoreTerms_ret = new Vector<ScoredTerms>();
+		
 		int tot=0;
 		for(i=0;i<numTerms;i++)
 			tot+= scoreTerms.get(i).get_score();
 		
+		System.out.println(tot);
 		for(i=0;i<numTerms;i++)
 		{
 			scoreTerms.get(i).set_score(scoreTerms.get(i).get_score()/tot);
